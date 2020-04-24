e0753b186ad6a6bd270c7d4350b404e599b2bb27
klickers <bluebirid228@gmail.com>
2020-04-24 16:14:26 -0700

    Initial commit

---
+++ contracts/FlightSuretyData.sol
@@ -0,0 +1,211 @@
+pragma solidity >=0.4.25;
+
+import "../node_modules/@openzeppelin/contracts/math/SafeMath.sol";
+
+contract FlightSuretyData {
+    using SafeMath for uint256;
+
+    /********************************************************************************************/
+    /*                                       DATA VARIABLES                                     */
+    /********************************************************************************************/
+
+    address private contractOwner;                                      // Account used to deploy contract
+    bool private operational = true;                                    // Blocks all state changes throughout the contract if false
+
+    struct AirlineProfile {
+        bool isRegistered;
+    }
+
+    uint constant M = 2;
+    address[] multiCalls = new address[](0);
+    mapping(address => AirlineProfile) airlineProfiles;   // Mapping for storing airline profiles
+
+    /********************************************************************************************/
+    /*                                       EVENT DEFINITIONS                                  */
+    /********************************************************************************************/
+
+
+    /**
+    * @dev Constructor
+    *      The deploying account becomes contractOwner
+    */
+    constructor
+                                (
+                                )
+                                public
+    {
+        contractOwner = msg.sender;
+    }
+
+    /********************************************************************************************/
+    /*                                       FUNCTION MODIFIERS                                 */
+    /********************************************************************************************/
+
+    // Modifiers help avoid duplication of code. They are typically used to validate something
+    // before a function is allowed to be executed.
+
+    /**
+    * @dev Modifier that requires the "operational" boolean variable to be "true"
+    *      This is used on all state changing functions to pause the contract in
+    *      the event there is an issue that needs to be fixed
+    */
+    modifier requireIsOperational()
+    {
+        require(operational, "Contract is currently not operational");
+        _;  // All modifiers require an "_" which indicates where the function body will be added
+    }
+
+    /**
+    * @dev Modifier that requires the "ContractOwner" account to be the function caller
+    */
+    modifier requireContractOwner()
+    {
+        require(msg.sender == contractOwner, "Caller is not contract owner");
+        _;
+    }
+
+    /********************************************************************************************/
+    /*                                       UTILITY FUNCTIONS                                  */
+    /********************************************************************************************/
+
+    /**
+    * @dev Get operating status of contract
+    *
+    * @return A bool that is the current operating status
+    */
+    function isOperational()
+                            public
+                            view
+                            returns(bool)
+    {
+        return operational;
+    }
+
+
+    /**
+    * @dev Sets contract operations on/off
+    *
+    * When operational mode is disabled, all write transactions except for this one will fail
+    */
+    function setOperatingStatus
+                            (
+                                bool mode
+                            )
+                            external
+                            requireContractOwner
+    {
+        require(mode != operational, "New mode must be different from existing mode");
+        require(airlineProfiles[msg.sender].isRegistered, "Caller is not an admin");
+
+        bool isDuplicate = false;
+        for(uint c=0; c<multiCalls.length; c++) {
+            if (multiCalls[c] == msg.sender) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        require(!isDuplicate, "Caller has already called this function.");
+
+        multiCalls.push(msg.sender);
+        if (multiCalls.length >= M) {
+            operational = mode;
+            multiCalls = new address[](0);
+        }
+        operational = mode;
+    }
+
+    /********************************************************************************************/
+    /*                                     SMART CONTRACT FUNCTIONS                             */
+    /********************************************************************************************/
+
+   /**
+    * @dev Add an airline to the registration queue
+    *      Can only be called from FlightSuretyApp contract
+    *
+    */
+    function registerAirline
+                            (
+                            )
+                            external
+                            pure
+    {
+
+    }
+
+
+   /**
+    * @dev Buy insurance for a flight
+    *
+    */
+    function buy
+                            (
+                            )
+                            external
+                            payable
+    {
+
+    }
+
+    /**
+     *  @dev Credits payouts to insurees
+    */
+    function creditInsurees
+                                (
+                                )
+                                external
+                                pure
+    {
+    }
+
+
+    /**
+     *  @dev Transfers eligible payout funds to insuree
+     *
+    */
+    function pay
+                            (
+                            )
+                            external
+                            pure
+    {
+    }
+
+   /**
+    * @dev Initial funding for the insurance. Unless there are too many delayed flights
+    *      resulting in insurance payouts, the contract should be self-sustaining
+    *
+    */
+    function fund
+                            (
+                            )
+                            public
+                            payable
+    {
+    }
+
+    function getFlightKey
+                        (
+                            address airline,
+                            string memory flight,
+                            uint256 timestamp
+                        )
+                        pure
+                        internal
+                        returns(bytes32)
+    {
+        return keccak256(abi.encodePacked(airline, flight, timestamp));
+    }
+
+    /**
+    * @dev Fallback function for funding smart contract.
+    *
+    */
+    function()
+                            external
+                            payable
+    {
+        fund();
+    }
+
+
+}
